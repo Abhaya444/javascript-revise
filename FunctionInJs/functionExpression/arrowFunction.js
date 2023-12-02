54869e44784c493f8daa8ba4f3f6a5bde17659ae
Abhaya Parida <115058980+Abhaya444@users.noreply.github.com>
2023-12-03 01:05:49 +0530

    code update

--- FunctionInJs/functionExpression/arrowFunction.js
+++ FunctionInJs/functionExpression/arrowFunction.js
@@ -7,4 +7,9 @@ var prod  = (num1, num2) => {
     return num1*num2;
 }
 //var prod = (num1, num2) => num1*num2;
-console.log(prod(2, 4));
+// console.log(prod(2, 4));
+
+const result =prod(2, 4);
+console.log(result);
+console.log("Hello")
+
