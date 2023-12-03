a079b1759a9c00f09d4ace1ca156ac5ea34703be
Abhaya Parida <115058980+Abhaya444@users.noreply.github.com>
2023-12-03 22:48:06 +0530

    callback, function as argument

---
+++ FunctionInJs/callbackFunction/callback.js
@@ -0,0 +1,18 @@
+function greet(wish){
+    console.log(`${wish}, welcome to the JavaScript Course`);
+}
+
+function sayHi(){
+    return "Hi";
+}
+
+function sayHello(){
+    return 'Hello';
+}
+function goodMorning(){
+    return 'Good Morning';
+}
+
+greet(sayHi());
+greet(sayHello());
+greet(goodMorning());
