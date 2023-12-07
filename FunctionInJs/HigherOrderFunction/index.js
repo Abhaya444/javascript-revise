77ea4f0a085119befdd1115b3b5785143bf11e89
Abhaya Parida <115058980+Abhaya444@users.noreply.github.com>
2023-12-07 20:41:01 +0530

    updated

--- FunctionInJs/HigherOrderFunction/index.js
+++ FunctionInJs/HigherOrderFunction/index.js
@@ -1,13 +1,45 @@
-//HOF is a function which accepting function as an argument
-//Or a function which returns function as an output 
+// //HOF is a function which accepting function as an argument
+// //Or a function which returns function as an output 
 
 const inputs = [1, 2, 3, 3, 4, 5, 6, 7, 19];
 
-function square(input){
-    const squared = [];
+
+//1st approach
+
+
+// function square(input){
+//     const squared = [];
+//     for(let num of input){
+//         squared.push(num * num);
+//     }
+//     return squared;
+// }
+// console.log(square(inputs));
+
+// function cubic(input){
+//     const cubes = [];
+//     for(let num of inputs){
+//         cubes.push(num*num*num)
+//     }
+//     return cubes;
+// }
+// const result = cubic(inputs);
+// console.log(result);
+
+//2nd approach
+
+function operation(input, fn){
+    const output = [];
     for(let num of input){
-        squared.push(num * num);
+        output.push(fn(num));
     }
-    return squared;
+    return output;
 }
-console.log(square(inputs));
+function square(number){
+    return number*number;
+}
+function cube(number){
+    return number*number*number;
+}
+console.log(operation(inputs, square));
+console.log(operation(inputs, cube));
