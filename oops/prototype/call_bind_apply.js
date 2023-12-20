54724523fd3adba2188dd4ea4486db2e81b2e776
Abhaya Parida <115058980+Abhaya444@users.noreply.github.com>
2023-12-20 17:03:21 +0530

    call bind apply prototype

--- oops/prototype/call_bind_apply.js
+++ oops/prototype/call_bind_apply.js
@@ -22,3 +22,14 @@ car.getDetails.apply(bus, ['Golld', 50]);
 const veh1 = car.getDetails.bind(bus);
 veh1('silver', 20);
 veh1('bronze', 20);
+
+//question
+function introduce(city, country){
+    console.log(this.name + "is from " + city + ", " + country);
+}
+
+let person = {name: "Alice"};
+
+introduce.call(person, "New York", "USA")
+introduce.apply(person, ["New York", "USA"])
+introduce.bind(person, "New York", "USA")
