576adbae021076cde00d468d867d7357b65e491d
Abhaya Parida <115058980+Abhaya444@users.noreply.github.com>
2023-12-24 23:51:53 +0530

    dom added

--- DOM/BasicsDOM/dom.js
+++ DOM/BasicsDOM/dom.js
@@ -4,3 +4,22 @@ btn.className = 'btn1';
 
 const division = document.querySelector('div');
 division.appendChild(btn);
+//remove element
+
+// document.querySelector('h3').remove();
+
+//Removing heeading in the click of a button
+btn.addEventListener('click', () => {
+    document.querySelector('h3').remove()
+})
+
+//Event Listener on Para
+const p = document.querySelector('p');
+p.addEventListener('mouseover', function(){
+    para.style.backgroundColor = "yellow";
+})
+
+
+p.addEventListener('mousedown', function(){
+    para.style.backgroundColor = "";
+})
